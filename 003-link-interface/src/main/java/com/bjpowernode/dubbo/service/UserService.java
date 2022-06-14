9598ad5e0a4af7a9983c4d1017f0fff12118ff40
ljn <742277203@qq.com>
2022-06-14 13:51:49 +0800

    first

---
+++ 003-link-interface/src/main/java/com/bjpowernode/dubbo/service/UserService.java
@@ -0,0 +1,9 @@
+package com.bjpowernode.dubbo.service;
+
+import com.bjpowernode.dubbo.model.User;
+
+public interface UserService {
+    User queryUserById(Integer id);
+
+    Integer queryAllUserCount();
+}
